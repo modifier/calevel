415c231292a681e52c52f79dbee469f21aafbfe6
Evgeniy Amiraslanov <the.modifikator@gmail.com>
2022-11-22 23:09:52 +0500

    Set target to es2020

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    target: "es2020",
     lib: {
-      entry: "src/ca-app.ts",
+      entry: "src/components/ca-app.ts",
       formats: ["es"],
     },
     rollupOptions: {
